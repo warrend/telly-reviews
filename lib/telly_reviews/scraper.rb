8a06cd71d88221e08e25aa0b2be8a263f53ae6a8
warrend <warrend555@gmail.com>
2016-12-30 16:45:48 -0500

    works on Scraper.make_list - refines css

--- lib/telly_reviews/scraper.rb
+++ lib/telly_reviews/scraper.rb
@@ -3,9 +3,9 @@ class TellyReviews::Scraper
   def self.make_list
     Nokogiri::HTML(open("http://variety.com/v/tv/reviews/")).css(".module-blogroll-section-front .story").each do |review|
       TellyReviews::Review.new({
-        title: review.css("h2 a").text.strip,
-        author: review.css(".author").text.strip
-        #url: review.css("h2 a").attr('href').value
+        title: review.css("h2").text.strip,
+        author: review.css(".author").text.strip,
+        url: review.css("a").attr('href').value
       })
     end
   end
