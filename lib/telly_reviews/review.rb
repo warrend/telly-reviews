8a294cf6dbfa93697ab5d37cacb7d1f9871d6653
warrend <warrend555@gmail.com>
2016-12-30 14:25:34 -0500

    adds make_list to scraper.rb

--- lib/telly_reviews/review.rb
+++ lib/telly_reviews/review.rb
@@ -1,6 +1,8 @@
 class TellyReviews::Review 
   attr_accessor :title, :author, :date, :body, :author_twitter
 
+  @@all = []
+
   
 
 end
--- lib/telly_reviews/scraper.rb
+++ lib/telly_reviews/scraper.rb
@@ -1,3 +1,10 @@
 class TellyReviews::Scraper 
   
+  def self.make_list
+    Nokogiri::HTML(open("http://variety.com/v/tv/reviews/")).css("story").each do |review|
+      TellyReviews::Review.new({
+        title: review.css("h2 a").text,
+        author: review.css(".author").text
+      })
+  end
 end
