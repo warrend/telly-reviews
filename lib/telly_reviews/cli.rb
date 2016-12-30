f1f6f790197eb4bb68ef5cd9b9bb9abe566a6597
warrend <warrend555@gmail.com>
2016-12-30 15:51:57 -0500

    adds input logic to CLI#start

--- lib/telly_reviews/cli.rb
+++ lib/telly_reviews/cli.rb
@@ -7,12 +7,38 @@ class TellyReviews::CLI
   end
 
   def start 
-    
+    print_list
+    puts "Enter number to see article:"
+    input = gets.strip
+
+    review = TellyReviews::Review.find(input.to_i)
+    print_review(review)
+    puts ""
+    puts "Would you like to read another review? (y/n)"
+    input = gets.strip.downcase
+    if input == 'y'
+      puts ""
+      start
+    else
+      puts "Please come back soon for the updated reviews!"
+      exit
+    end
   end
 
   def print_list
     TellyReviews::Reviews.all.each.with_index(1) do |review, index|
       puts "#{index}. #{review.body} by #{review.author}"
     end
+  end
+
+  def print_review(review)
+    puts "#{review.title}"
+    puts ""
+    puts "By #{review.author}"
+    puts "#{review.date}"
+    puts "Twitter: #{review.author_twitter}"
+    puts ""
+    puts review.body
+    puts ""
   end
 end
